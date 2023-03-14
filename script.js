a77298cea358a9980328ece25f2f271b611a16fb
jproduct74 <126370953+jproduct74@users.noreply.github.com>
2023-03-13 23:39:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,47 @@
-// How the script works, in steps
-// X: 12123.35, Y: 12644.46, Z: 161.15
-// X: 12123.35, Y: 12644.46
-// 12123.35, Y: 12644.46
-// 12123.35, 12644.46
-// 12123.35,1,12644.46
+let input, firstLetter, secondLetter, newStr, noX, removeY, result, letter, index;
+let addOneString, indexOne, addOneL, finalResultString, final;
 
-input = prompt("Enter the coordinates exactly how you copy them from CF(X: .. Y: .. Z: ..");
-let str = input;
-const firstLetter = str.indexOf(",");
-const secondLetter = str.indexOf(",", firstLetter + 1);
-const newStr = str.slice(0, secondLetter);
+function fixCoords() {
+  input = prompt("Enter the coordinates exactly how you copy them from CF(X: .. Y: .. Z: ..");
 
-function fixCoords(input) {
-  console.log(newStr);
-  removeXAxis();
-  addOne();
-  finalResult();
-  reloadPage();
-  return newStr;
+  firstLetter = input.indexOf(",");
+  secondLetter = input.indexOf(",", firstLetter + 1);
+  newStr = input.slice(0, secondLetter);
+
+  noX = newStr.slice(3);
+
+  removeY = noX;
+  letter = "Y";
+  index = removeY.indexOf(letter);
+
+  if (index !== -1) {
+    result = removeY.slice(0, index - 1) + removeY.slice(index + 2);
+  }
+
+  addOneString = result;
+  indexOne = addOneString.indexOf(",");
+  addOneL = addOneString.slice(0, indexOne + 1) + "1," + addOneString.slice(indexOne + 1);
+
+  finalResultString = addOneL;
+  final = finalResultString.replace(/\s/g, "");
+
+  const outputElement = document.createElement('p');
+  outputElement.innerText = `Coords: ${final}`;
+  document.body.appendChild(outputElement);
+
+  const copyButton = document.createElement('button');
+  copyButton.innerText = 'Copy to clipboard';
+  copyButton.classList.add("copybutton");
+  copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(final)
+      .then(() => {
+        alert('Copied to clipboard!');
+      })
+      .catch((error) => {
+        console.error('Failed to copy: ', error);
+      });
+  });
+
+  document.body.appendChild(copyButton);
 }
 
-const noX = newStr.slice(3);
-
-function removeXAxis(ainput) {
-  console.log(noX);
-  return noX;
-}
-
-let removeY = noX;
-let result;
-let letter = "Y";
-let index = removeY.indexOf(letter);
-
-if (index !== -1) {
-  result = removeY.slice(0, index - 1) + removeY.slice(index + 2);
-}
-console.log(result);
-
-const addOneString = result;
-const indexOne = addOneString.indexOf(",");
-const addOneL = addOneString.slice(0, indexOne + 1) + "1," + addOneString.slice(indexOne + 1);
-
-function addOne(cinput) {
-  console.log(addOneL);
-}
-
-const finalResultString = addOneL;
-const final = finalResultString.replace(/\s/g, "");
-
-function finalResult(dinput) {
-  alert("Coords: " + final);
-}
-
-function reloadPage() {
-  location.reload();
-}
-
-fixCoords();
